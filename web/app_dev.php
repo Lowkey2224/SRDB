3594fbfe099627f0d0d1ccd44252ab0dbab25cf5
jenz <jenz@silpion.de>
2016-04-01 20:09:35 +0200

    added more allowed IPs do app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,6 +13,7 @@ $allowedIps = array('127.0.0.1',
     'fe80::1',
     '::1',
     '213.209.99.169', // Silpion Offics
+    ' 31.18.158.60', // Loki@Home
 );
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
